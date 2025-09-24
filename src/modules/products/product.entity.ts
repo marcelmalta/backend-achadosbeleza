49bd7046a52b5a880949b07c7e6884ca224bb458
marcelmalta <marcelmalta@hotmail.com>
2025-09-24 11:28:21 -0300

    fix: altera Product.id para UUID string e ajusta service

--- src/modules/products/product.entity.ts
+++ src/modules/products/product.entity.ts
@@ -2,8 +2,8 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 @Entity("products")
 export class Product {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
   @Column()
   title: string;
